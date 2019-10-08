b192d4065c61b7fedc198eee11f3f6b274209580
Norman Hong <nh530@georgetown.edu>
2019-10-08 14:07:48 -0400

    added evaluation of classifier on validation set

--- part2_norman.py
+++ part2_norman.py
@@ -2,16 +2,23 @@ import re
 import pandas as pd 
 from nltk.classify import NaiveBayesClassifier
 from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
 
-data_dev = pd.read_csv("Gold/dev.txt", sep='\t', header=None, 
+
+data_dev = pd.read_csv("Gold/dev.txt", sep='\t', header=None, index_col=False,
                    names=['id', 'target', 'tweet'], encoding='utf-8')
-data_dev.drop(['id',], axis=1, inplace=True)
+data_dev.drop(['id'], axis=1, inplace=True)
 
-data_train = pd.read_csv("Gold/train.txt", sep='\t', header=None, 
+data_train = pd.read_csv("Gold/train.txt", sep='\t', header=None, index_col=False,
                    names=['id', 'target', 'tweet'], encoding='utf-8')
-data_train.drop(['id',], axis=1, inplace=True)
+data_train.drop(['id'], axis=1, inplace=True)
 
+data_devtest = pd.read_csv("Gold/devtest.txt", sep='\t', header=None, index_col=False,
+                           names=['id', 'target', 'tweet'], encoding='utf-8')
+data_devtest.drop(['id'], axis=1, inplace=True)
+
+data_test = pd.read_csv("Gold/test.txt", sep='\t', header=None, index_col=False,
+                           names=['id', 'target', 'tweet'], encoding='utf-8')
+data_test.drop(['id'], axis=1, inplace=True)
 
 
 regexes=(
@@ -71,29 +78,67 @@ big_url_pattern = "|".join(url_pattern)
 url_tokenizer = re.compile(big_url_pattern, re.VERBOSE | re.I | re.UNICODE)
 
 
-dev_tokens = []
-for text in data_dev.values:
+dev_tokens = [] # stores all features for dev dataset.  
+for text in data_dev.values: # for each tweet.  
+    temp = []  # stores the matches for a single tweet
     for matches in my_extensible_tokenizer.findall(text[1]):
         # determine if matches is a url.  
         url_matches = url_tokenizer.findall(matches)
         # if the match is a url, then won't add to temp.  
         if not url_matches:
-            dev_tokens.append(({'contains(' + matches + ')': True}, text[0]))
+            temp.append(('contains(' + matches + ')', True))
+        dev_tokens.append((dict(temp), text[0]))
 
-train_tokens = []
-for text in data_train.values:
+train_tokens = [] # stores all features for train dataset.  
+for text in data_train.values: # for each tweet.  
+    temp = [] # stores tokens for a given tweet.  
     for matches in my_extensible_tokenizer.findall(text[1]):
         # determine if matches is a url.  
         url_matches = url_tokenizer.findall(matches)
         # if the match is a url, then won't add to temp.  
         if not url_matches:
-            train_tokens.append(({'contains(' + matches + ')':True}, text[0]))
-            
+            temp.append(('contains(' + matches + ')', True))
+        train_tokens.append((dict(temp), text[0]))
+
+
 training_features = train_tokens + dev_tokens
 
 sentiment_analyzer = SentimentAnalyzer()
 trainer = NaiveBayesClassifier.train
-classifier = sentiment_analyzer.train(trainer, training_features)
+classifier = sentiment_analyzer.train(trainer=trainer, training_set=training_features)
+
+# Evaluating model on training data.
+sentiment_analyzer.evaluate(training_features, classifier)
+
+
+devtest_tokens = [] # stores all features for devtest dataset.  
+for text in data_devtest.values: # for each tweet.  
+    temp = []  # stores the matches for a single tweet
+    for matches in my_extensible_tokenizer.findall(text[1]):
+        # determine if matches is a url.  
+        url_matches = url_tokenizer.findall(matches)
+        # if the match is a url, then won't add to temp.  
+        if not url_matches:
+            temp.append(('contains(' + matches + ')', True))
+        devtest_tokens.append((dict(temp), text[0]))
+
+test_tokens = [] # stores all features for train dataset.  
+for text in data_test.values: # for each tweet.  
+    temp = [] # stores tokens for a given tweet.  
+    for matches in my_extensible_tokenizer.findall(text[1]):
+        # determine if matches is a url.  
+        url_matches = url_tokenizer.findall(matches)
+        # if the match is a url, then won't add to temp.  
+        if not url_matches:
+            temp.append(('contains(' + matches + ')', True))
+        test_tokens.append((dict(temp), text[0]))
+
+test_final = devtest_tokens + test_tokens
+
+sentiment_analyzer.evaluate(test_final, classifier)
+
+
+
 
 
 
