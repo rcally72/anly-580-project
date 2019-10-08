4ad2031de7f8263f4465b8aea0a46417953cda1b
Norman Hong <nh530@georgetown.edu>
2019-10-08 10:10:36 -0400

    Added nltk sentiment model.

--- part2_norman.py
+++ part2_norman.py
@@ -1,10 +1,16 @@
 import re 
 import pandas as pd 
+from nltk.classify import NaiveBayesClassifier
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
 
-
-data = pd.read_csv("Gold/dev.txt", sep='\t', header=None, 
+data_dev = pd.read_csv("Gold/dev.txt", sep='\t', header=None, 
                    names=['id', 'target', 'tweet'], encoding='utf-8')
-data.drop(['id',], axis=1, inplace=True)
+data_dev.drop(['id',], axis=1, inplace=True)
+
+data_train = pd.read_csv("Gold/train.txt", sep='\t', header=None, 
+                   names=['id', 'target', 'tweet'], encoding='utf-8')
+data_train.drop(['id',], axis=1, inplace=True)
 
 
 
@@ -64,20 +70,30 @@ r'www\.[a-zA-Z0-9]+\.[^\s]{2,})',
 big_url_pattern = "|".join(url_pattern)
 url_tokenizer = re.compile(big_url_pattern, re.VERBOSE | re.I | re.UNICODE)
 
-temp = []
-for text in data.values:
+
+dev_tokens = []
+for text in data_dev.values:
     for matches in my_extensible_tokenizer.findall(text[1]):
         # determine if matches is a url.  
         url_matches = url_tokenizer.findall(matches)
         # if the match is a url, then won't add to temp.  
         if not url_matches:
-            temp.append((matches, text[0]))
+            dev_tokens.append(({'contains(' + matches + ')': True}, text[0]))
+
+train_tokens = []
+for text in data_train.values:
+    for matches in my_extensible_tokenizer.findall(text[1]):
+        # determine if matches is a url.  
+        url_matches = url_tokenizer.findall(matches)
+        # if the match is a url, then won't add to temp.  
+        if not url_matches:
+            train_tokens.append(({'contains(' + matches + ')':True}, text[0]))
             
+training_features = train_tokens + dev_tokens
 
-
-    
-
-
+sentiment_analyzer = SentimentAnalyzer()
+trainer = NaiveBayesClassifier.train
+classifier = sentiment_analyzer.train(trainer, training_features)
 
 
 
